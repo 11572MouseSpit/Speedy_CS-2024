95a770f5eb886ae6a3ab733ef051636e4a3127ca
ctsmith6828 <christopher@sixbyte.com>
2024-01-08 12:31:33 -0600

    Updates to SDK

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/SpeedyTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/SpeedyTeleOp.java
@@ -6,13 +6,12 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.Hardware.HWProfile;
 import org.firstinspires.ftc.teamcode.Libs.DriveClass;
 
 @TeleOp(name = "Field Centric", group = "Competition")
 
-public class JoslynTeleOp extends LinearOpMode {
+public class SpeedyTeleOp extends LinearOpMode {
     private final static HWProfile robot = new HWProfile();
 
     @Override
